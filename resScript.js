e7838481b89a24611af513403256969bd2a6c50b
Caleb Campbell <38718983+MrChido@users.noreply.github.com>
2019-02-10 17:36:25 -0600

    trying to alternate colors

---
+++ resScript.js
@@ -0,0 +1,15 @@
+// asset link
+
+const foUsed = document.getElementById('foButt');
+const soUsed = document.getElementById('soButt');
+const datUsed = document.getElementById('datButt');
+const ldUsed = document.getElementById('ldButt');
+
+//acton center
+
+foUsed.addEventListener('click',()=>{
+	//color check
+	if (foTitle.color=='rgb(252,104,84)'){
+		foTitle.color='rgb(106,127,194)'
+	}
+});
